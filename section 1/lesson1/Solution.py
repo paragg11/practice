9f2f374836ed8635163dcb7cc4f5953be95bca08
paragg11 <paraggunjal01@gmail.com>
2022-12-04 17:50:51 +0530

    variables and functions.

---
+++ section 1/lesson1/Solution.py
@@ -0,0 +1,20 @@
+if __name__ == "__main__":
+
+    # VARIABLES ASSIGNED TO WORDS IN STRING FORMAT
+
+    player_choice = "rock"
+    print(player_choice)
+
+    computer_choice = "paper"
+    print(computer_choice)
+
+    # FUNCTIONS
+    def get_choices():
+        player_choice = "kohli"
+        computer_choice = "dhoni"
+
+        return computer_choice
+
+    choices = get_choices()
+    print(choices)
+
