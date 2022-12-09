9a935b5c796b1695cb2597fe13426795fdcbc3c6
paragg11 <paraggunjal01@gmail.com>
2022-12-09 17:08:05 +0530

    filter function.

--- section 1/lesson1/Solution.py
+++ section 1/lesson1/Solution.py
@@ -638,7 +638,16 @@ if __name__ == "__main__":
 
     print(list(result1))                # [2, 4, 6]
 
+    # FILTERING FUNCTION
 
+    new_numbers = [1, 2, 3, 4, 5, 6]
+
+    def is_even(n):
+        return n % 2 == 0
+
+    result2 = filter(is_even, new_numbers)
+
+    print(list(result2))                    # [2, 4, 6]
 
 
 
