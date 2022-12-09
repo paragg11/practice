91e345cd175b6a29adb0c244b265d9933ac16640
paragg11 <paraggunjal01@gmail.com>
2022-12-09 16:38:26 +0530

    lambda function.

--- section 1/lesson1/Solution.py
+++ section 1/lesson1/Solution.py
@@ -611,3 +611,19 @@ if __name__ == "__main__":
     from math import sqrt
 
     print(sqrt(16))                         # 4.0
+
+    # ACCEPTING ARGUMENTS
+
+
+    # LAMBDA FUNCTIONS      -> lambda argument: single expression (expression returns a value)
+
+    lambda num : num * 2
+
+    multiply = lambda a, b : a * b
+
+    print(multiply(2 , 3))              # 6
+
+
+
+
+
