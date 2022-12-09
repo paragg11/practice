cd492db0dabf0a65b4c9d90cbf75245b98175a8e
paragg11 <paraggunjal01@gmail.com>
2022-12-09 16:55:53 +0530

    map function.

--- section 1/lesson1/Solution.py
+++ section 1/lesson1/Solution.py
@@ -623,6 +623,18 @@ if __name__ == "__main__":
 
     print(multiply(2 , 3))              # 6
 
+    # MAP, FILTER AND REDUCE
+
+    numbers = [1, 2, 3]
+
+    def double(a):
+        return a * 2
+
+    result = map(double, numbers)
+
+    print(list(result))                 # [2, 4, 6]
+
+
 
 
 
