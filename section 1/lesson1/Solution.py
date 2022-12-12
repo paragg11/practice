ee8c91d0a8a16c30d99177438488f0c7f9ff8a71
paragg11 <paraggunjal01@gmail.com>
2022-12-13 00:36:01 +0530

    list compressions.

--- section 1/lesson1/Solution.py
+++ section 1/lesson1/Solution.py
@@ -753,4 +753,10 @@ if __name__ == "__main__":
 
     # pip uninstall requests
 
+    # LISTS COMPRESSIONS
 
+    numbers = [1, 2, 3, 4, 5]
+
+    numbers_power_2 = [n**2 for n in numbers]
+
+    print(numbers_power_2)                  # [1, 4, 9, 16, 25]
