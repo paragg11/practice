9c7f0c7793965b4ddf19fb335c72c80509fa9798
paragg11 <paraggunjal01@gmail.com>
2022-12-13 14:42:33 +0530

    refactoring - 4 - adding function.

--- section 1/lesson1/blackjack.py
+++ section 1/lesson1/blackjack.py
@@ -6,13 +6,21 @@ suits = ["spades", "clubs", "hearts", "diamonds", "snakes"]
 
 ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 
-
 for suit in suits:
     for rank in ranks:
         cards.append([suit, rank])
 
-random.shuffle(cards)
 
-card = cards.pop()
+def shuffle():
+    random.shuffle(cards)
 
-print(card)
+
+shuffle()
+
+
+def deal():
+    card = cards.pop()
+    return card
+
+
+print(deal())
