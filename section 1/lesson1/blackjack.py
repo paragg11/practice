a1e7da6f7d100c071ee4a87ed8e9ffb86c683746
paragg11 <paraggunjal01@gmail.com>
2022-12-13 14:24:49 +0530

    refactoring 1 - starting backjack with list, string concatenation and for loop.

---
+++ section 1/lesson1/blackjack.py
@@ -0,0 +1,9 @@
+suits = ["spades", "clubs", "hearts", "diamonds", "snakes"]
+suit = suits[2]
+rank = "K"
+value = 10
+
+print("Your card is: " + rank + " of " + suit)
+
+for suit in suits:
+    print(suit)
