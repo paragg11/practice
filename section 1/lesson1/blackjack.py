022febb3ffdd281d8847e752da0b4c1e373efeeb
paragg11 <paraggunjal01@gmail.com>
2022-12-13 15:18:38 +0530

    refactoring - 7

--- section 1/lesson1/blackjack.py
+++ section 1/lesson1/blackjack.py
@@ -4,7 +4,21 @@ cards = []
 
 suits = ["spades", "clubs", "hearts", "diamonds", "snakes"]
 
-ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+ranks = [
+            {"rank": "A", "value": 11},
+            {"rank": "2", "value": 2},
+            {"rank": "3", "value": 3},
+            {"rank": "4", "value": 4},
+            {"rank": "5", "value":  5},
+            {"rank": "6", "value":  6},
+            {"rank": "7", "value":  7},
+            {"rank": "8", "value":  8},
+            {"rank": "9", "value":  9},
+            {"rank": "10", "value":  10},
+            {"rank": "J", "value":  10},
+            {"rank": "Q", "value": 10},
+            {"rank": "K", "value":  10},
+         ]
 
 for suit in suits:
     for rank in ranks:
@@ -26,18 +40,8 @@ def deal(number):
     return cards_dealt
 
 
-cards_dealth = deal(2)
-card = cards_dealth[0]
-rank = card[1]
-
-if rank == "A":
-    value = 11
-elif rank == "J" or rank == "Q" or rank == "K":
-    value = 10
-else:
-    value = rank
-
-rank_dict = {"rank": rank, "value": value}
-print(rank_dict["rank"], rank_dict["value"])
+card = deal(1)[0]
+print(card)
+print(card[1]["value"])
 
 
