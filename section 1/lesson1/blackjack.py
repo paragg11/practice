92ff255a26a29588bc8f8ceaf9e959c9ec783d18
paragg11 <paraggunjal01@gmail.com>
2022-12-13 15:58:18 +0530

    refactoring - 8

--- section 1/lesson1/blackjack.py
+++ section 1/lesson1/blackjack.py
@@ -1,47 +1,47 @@
 import random
 
-cards = []
 
-suits = ["spades", "clubs", "hearts", "diamonds", "snakes"]
+class Deck:
+    def __init__(self):
 
-ranks = [
-            {"rank": "A", "value": 11},
-            {"rank": "2", "value": 2},
-            {"rank": "3", "value": 3},
-            {"rank": "4", "value": 4},
-            {"rank": "5", "value":  5},
-            {"rank": "6", "value":  6},
-            {"rank": "7", "value":  7},
-            {"rank": "8", "value":  8},
-            {"rank": "9", "value":  9},
-            {"rank": "10", "value":  10},
-            {"rank": "J", "value":  10},
-            {"rank": "Q", "value": 10},
-            {"rank": "K", "value":  10},
-         ]
+        self.cards = []
 
-for suit in suits:
-    for rank in ranks:
-        cards.append([suit, rank])
+        suits = ["spades", "clubs", "hearts", "diamonds", "snakes"]
+
+        ranks = [
+                    {"rank": "A", "value": 11},
+                    {"rank": "2", "value": 2},
+                    {"rank": "3", "value": 3},
+                    {"rank": "4", "value": 4},
+                    {"rank": "5", "value":  5},
+                    {"rank": "6", "value":  6},
+                    {"rank": "7", "value":  7},
+                    {"rank": "8", "value":  8},
+                    {"rank": "9", "value":  9},
+                    {"rank": "10", "value":  10},
+                    {"rank": "J", "value":  10},
+                    {"rank": "Q", "value": 10},
+                    {"rank": "K", "value":  10},
+                 ]
+
+        for suit in suits:
+            for rank in ranks:
+                self.cards.append([suit, rank])
+
+    def shuffle(self):
+        random.shuffle(self.cards)
+
+    def deal(self, number):
+        cards_dealt = []
+        for x in range(number):
+            card = self.cards.pop()
+            cards_dealt.append(card)
+        return cards_dealt
 
 
-def shuffle():
-    random.shuffle(cards)
-
-
-shuffle()
-
-
-def deal(number):
-    cards_dealt = []
-    for x in range(number):
-        card = cards.pop()
-        cards_dealt.append(card)
-    return cards_dealt
-
-
-card = deal(1)[0]
-print(card)
-print(card[1]["value"])
-
+deck1 = Deck()
+deck2 = Deck()
+deck2.shuffle()
+print(deck1.cards)
+print(deck2.cards)
 
