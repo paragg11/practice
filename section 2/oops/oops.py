b49f25dceb222610b4fb8ecefb4fac9b61b57a55
paragg11 <paraggunjal01@gmail.com>
2022-12-16 00:14:31 +0530

    add calculate_total_price method in class.

--- section 2/oops/oops.py
+++ section 2/oops/oops.py
@@ -1,9 +1,7 @@
-item1 = "phone"
-item1_price = 100
-item1_quantity = 5
-item1_price_total = item1_price * item1_quantity
+class Item:
+    def calculate_total_price(self):
+        pass
 
-print(type(item1))                  # <class 'str'>
-print(type(item1_price))            # <class 'int'>
-print(type(item1_quantity))         # <class 'int'>
-print(type(item1_price_total))      # <class 'int'>
+item1 = Item()
+item1.calculate_total_price()
+
