9bce6b55345f5f9743f1c35880029780eef5bbc5
paragg11 <paraggunjal01@gmail.com>
2022-12-16 00:25:31 +0530

    add arguments.

--- section 2/oops/oops.py
+++ section 2/oops/oops.py
@@ -1,7 +1,9 @@
 class Item:
-    def calculate_total_price(self):
-        pass
+    def calculate_total_price(self, x, y):
+        return x * y
 
 item1 = Item()
-item1.calculate_total_price()
+item1.price = 5
+item1.quantity = 100
+print(item1.calculate_total_price(item1.price, item1.quantity))
 
