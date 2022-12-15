a316eab547b9e5a0f9382be4a8beb290c2773512
paragg11 <paraggunjal01@gmail.com>
2022-12-15 23:51:09 +0530

    variables and its types.

---
+++ section 2/oops/oops.py
@@ -0,0 +1,9 @@
+item1 = "phone"
+item1_price = 100
+item1_quantity = 5
+item1_price_total = item1_price * item1_quantity
+
+print(type(item1))                  # <class 'str'>
+print(type(item1_price))            # <class 'int'>
+print(type(item1_quantity))         # <class 'int'>
+print(type(item1_price_total))      # <class 'int'>
