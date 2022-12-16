9645734005eabf73ccadaadb11fa03df12c0eaf4
paragg11 <paraggunjal01@gmail.com>
2022-12-16 16:01:15 +0530

    init method.

--- section 2/oops/oops.py
+++ section 2/oops/oops.py
@@ -1,9 +1,13 @@
 class Item:
+
+    def __init__(self):            # magic methods -> when instance is created, python calls init method first in class.
+        print("I M CREATED.")
+
     def calculate_total_price(self, x, y):
         return x * y
 
 item1 = Item()
 item1.price = 5
 item1.quantity = 100
-print(item1.calculate_total_price(item1.price, item1.quantity))
+#print(item1.calculate_total_price(item1.price, item1.quantity))
 
