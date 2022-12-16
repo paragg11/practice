e49cbf24e0c96ff8cee808caa2e9d5e7a88c6c17
paragg11 <paraggunjal01@gmail.com>
2022-12-16 16:11:59 +0530

    dynamically assign attributes.

--- section 2/oops/oops.py
+++ section 2/oops/oops.py
@@ -1,13 +1,17 @@
 class Item:
 
-    def __init__(self):            # magic methods -> when instance is created, python calls init method first in class.
-        print("I M CREATED.")
+    def __init__(self, name, price, quantity):            # magic methods -> when instance is created, python calls init method first in class.
+        self.name = name       # dynamically assign attributes
+        self.price = price
+        self.quantity = quantity
 
     def calculate_total_price(self, x, y):
         return x * y
 
-item1 = Item()
+
+item1 = Item("parag", 100, 5)
 item1.price = 5
 item1.quantity = 100
 #print(item1.calculate_total_price(item1.price, item1.quantity))
 
+print(item1.name)                       # parag
