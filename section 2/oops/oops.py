59df2b7ebbe3a3912b23a7f3c4d3a9287cd04794
paragg11 <paraggunjal01@gmail.com>
2022-12-17 12:35:40 +0530

    assertion error handling.

--- section 2/oops/oops.py
+++ section 2/oops/oops.py
@@ -1,17 +1,28 @@
 class Item:
 
-    def __init__(self, name, price, quantity):            # magic methods -> when instance is created, python calls init method first in class.
-        self.name = name       # dynamically assign attributes
+    def __init__(self, name: str, price: float, quantity=0):  # magic methods -> when instance is created, python calls init method \
+        # first in class.
+
+        # run validations to the received arguments
+
+        assert price >= 0, f"Price {price} is not greater than or equal to zero!"
+        assert quantity >= 0, f"Quantity {quantity} is not greater than or equal to zero!"
+
+        # assign to self object
+        self.name = name                                  # dynamically assign attributes
         self.price = price
         self.quantity = quantity
 
-    def calculate_total_price(self, x, y):
-        return x * y
+
+    def calculate_total_price(self):
+        return self.price * self.quantity
 
 
-item1 = Item("parag", 100, 5)
+item1 = Item("parag", -11, 10)          # AssertionError: Price -11 is not greater than or equal to zero!
 item1.price = 5
 item1.quantity = 100
-#print(item1.calculate_total_price(item1.price, item1.quantity))
+print(item1.calculate_total_price())
 
-print(item1.name)                       # parag
+
+
+
